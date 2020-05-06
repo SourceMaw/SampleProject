bea20cbe674d5fee90d8a4137b350aafa3c59e41
marius <mariusbac31@gmail.com>
2020-05-06 18:23:15 +0300

    adding admin page info and updating admin user email and password

--- module/Application/src/Controller/AdminController.php
+++ module/Application/src/Controller/AdminController.php
@@ -7,8 +7,10 @@
 
 namespace Application\Controller;
 
+use App\Auth;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
+use Models\User;
 
 class AdminController extends AbstractActionController
 {
@@ -23,5 +25,82 @@ class AdminController extends AbstractActionController
         }
 
         return new ViewModel();
+    }
+
+    public function admininfoAction(){
+
+        $userSession = \App\Auth::getAuth();
+
+        if(empty($userSession)){
+
+            header('Location: /login');
+            exit;
+        }
+
+        return new ViewModel();
+    }
+
+    public function updateAdminAction(){
+
+        $userSession = \App\Auth::getAuth();
+
+        if(empty($userSession)){
+
+            header('Location: /login');
+            exit;
+        }
+
+        $data = $this->params()->fromPost();
+
+        if(empty($data['email'])){
+
+            die('You need to fill the email');
+
+        } else if(strlen($data['email']) > 100) {
+
+            die('The email must be maximum 100 characters long');
+
+        } else if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+
+            die('You need to fill a valid email');
+
+        } else if (empty($data['actual-password'])) {
+
+            die('You need to fill the actual password');
+
+        } else if(!(Auth::hashPassword($data['actual-password']) == $userSession['user_password'])){
+
+            die('Your entered password does not match the actual password');
+
+        } else if (empty($data['new-password'])) {
+
+            die('You need to fill the new password');
+
+        } else if (strlen($data['new-password']) < 8) {
+
+            die('The new-password must be at least 8 characters long');
+
+        } else if (empty($data['confirm-password'])) {
+
+            die('You need to fill the confirm password');
+
+        } else if (!($data['confirm-password'] == $data['new-password'])) {
+
+            die('New Password and Confirm password must match!');
+        }
+
+        $serviceUser = new User();
+
+        $fields = [
+
+            'user_email' => $data['email'],
+            'user_password' => Auth::hashPassword($data['new-password'])
+        ];
+
+        $serviceUser->update($fields, $userSession['user_id']);
+
+        echo 'done';
+        exit;
+
     }
 }
