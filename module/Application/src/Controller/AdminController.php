f426b20afb48629bd81be4f9a8684152fc3e3354
marius <mariusbac31@gmail.com>
2020-05-06 22:40:30 +0300

    adding employee page and inserting employee

--- module/Application/src/Controller/AdminController.php
+++ module/Application/src/Controller/AdminController.php
@@ -10,6 +10,7 @@ namespace Application\Controller;
 use App\Auth;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
+use Models\Employee;
 use Models\User;
 
 class AdminController extends AbstractActionController
@@ -102,5 +103,79 @@ class AdminController extends AbstractActionController
         echo 'done';
         exit;
 
+    }
+
+    public function addemployeeAction(){
+
+        $userSession = \App\Auth::getAuth();
+
+        if(empty($userSession)){
+
+            header('Location: /login');
+            exit;
+        }
+
+        return new ViewModel();
+    }
+
+    public function addEmpAction(){
+
+        $userSession = \App\Auth::getAuth();
+
+        if(empty($userSession)){
+
+            header('Location: /login');
+            exit;
+        }
+
+        $data = $this->params()->fromPost();
+
+        if(empty($data['name'])){
+
+            die('You need to fill the name');
+
+        } else if(empty($data['position'])){
+
+            die('You need to fill the position');
+
+        } else if(empty($data['office'])){
+
+            die('You need to fill the office');
+
+        } else if(empty($data['age'])){
+
+            die('You need to fill the age');
+
+        } else if(empty($data['salary'])){
+
+            die('You need to fill the salary');
+
+        } else if(empty($data['email'])){
+
+            die('You need to fill the email');
+
+        } else if(strlen($data['email']) > 100) {
+
+            die('The email must be maximum 100 characters long');
+
+        } else if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+
+            die('You need to fill a valid email');
+
+        }
+
+        $serviceEmployee = new Employee();
+
+        if(!empty($serviceEmployee->getEmployeeByEmail($data['email']))){
+
+            die('This employee is already in your account');
+        }
+
+        $serviceEmployee->addEmployee($data['name'], $data['position'], $data['office'], $data['age'],
+            $data['salary'], $data['email']);
+
+
+        echo 'done';
+        exit;
     }
 }
---
+++ module/Models/src/Employee.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Models;
+
+use Laminas\Db\Sql\Delete;
+use Laminas\Db\Sql\Select;
+use Laminas\Db\Sql\Sql;
+
+class Employee extends Base{
+
+    public $tableName = 'employees';
+    public $pkId = 'emp_id';
+
+    public function addEmployee($name, $position, $office, $age, $salary, $email){
+
+        $existingEmployee = $this->getEmployeeByEmail($email);
+        $wasInserted = false;
+
+        $fields = [
+
+            'emp_name' => $name,
+            'emp_position' => $position,
+            'emp_office' => $office,
+            'emp_age' => $age,
+            'emp_salary' => $salary,
+            'emp_email' => $email
+
+        ];
+
+        if(empty($existingEmployee)){
+
+            $this->insert($fields);
+            $wasInserted = true;
+
+        }
+
+        return [
+
+            'wasInserted' => $wasInserted
+
+        ];
+
+    }
+
+    public function getEmployeeByEmail($email){
+
+        $select = $this->getSelectObject();
+        $select->where(
+            [
+                'emp_email' => $email
+            ]
+        );
+
+        $results = $this->runQueryBuilt($select);
+
+        if(!empty($results[0])){
+
+            return $results[0];
+        }
+
+        return $results;
+    }
+
+}
